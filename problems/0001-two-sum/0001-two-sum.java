8db8e61d4d15153d09c444cb3b5249f0bbddc439
Jitesh Singh <65043216+jksingh18@users.noreply.github.com>
2025-07-21 12:40:21 +0000

    Two Sum (Java)

--- problems/0001-two-sum/0001-two-sum.java
+++ problems/0001-two-sum/0001-two-sum.java
@@ -1,30 +1,12 @@
 class Solution {
-    
-    // Method to find two numbers in the array that add up to the target value
     public int[] twoSum(int[] nums, int target) {
-
-        // Create a HashMap to store numbers and their corresponding indices
-        Map<Integer, Integer> numToIndexMap = new HashMap<>();
-        
-        // Get the size of the input array
-        int sizeOfArray = nums.length;
-
-        // Loop through the array
-        for(int i = 0; i < sizeOfArray; i++) {
-            // Calculate the difference between the target and the current number
-            int diff = target - nums[i];
-
-            // Check if the difference already exists in the map
-            if(numToIndexMap.containsKey(diff)) {
-                // If it exists, return the indices of the current number and the number that adds up to the target
-                return new int[]{i, numToIndexMap.get(diff)};
+        HashMap<Integer, Integer> twoSumMap = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(twoSumMap.containsKey(target - nums[i])){
+                return new int[]{twoSumMap.get(target-nums[i]), i};
             }
-
-            // If it doesn't exist, add the current number and its index to the map
-            numToIndexMap.put(nums[i], i);
+            twoSumMap.put(nums[i], i);
         }
-
-        // If no two numbers add up to the target, return null
-        return null;
+        return new int[]{-1, -1};
     }
 }
