5a86affc7968be9ef05aadc83c66644cdef523ee
Jitesh Singh <65043216+jksingh18@users.noreply.github.com>
2025-08-01 08:15:15 +0000

    Pascal's Triangle (Java)

--- problems/0118-pascals-triangle/0118-pascals-triangle.java
+++ problems/0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,17 +1,27 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-		List<Integer> row, pre = null;
-		for (int i = 0; i < numRows; ++i) {
-			row = new ArrayList<Integer>();
-			for (int j = 0; j <= i; ++j)
-				if (j == 0 || j == i)
-					row.add(1);
-				else
-					row.add(pre.get(j - 1) + pre.get(j));
-			pre = row;
-			res.add(row);
-		}
-		return res;
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            // First element is always 1
+            row.add(1);
+
+            // For inner elements: sum of previous row's two elements above
+            for (int j = 1; j < i; j++) {
+                int prevRowLeft = triangle.get(i-1).get(j-1);
+                int prevRowRight = triangle.get(i-1).get(j);
+                row.add(prevRowLeft + prevRowRight);
+            }
+
+            // Last element is always 1 (except for first row)
+            if (i > 0) row.add(1);
+
+            triangle.add(row);
+        }
+
+        return triangle;
     }
 }
