d8b7105b93cd1d8b19e7ab64c28c6db181dfcd99
Jitesh Singh <65043216+jksingh18@users.noreply.github.com>
2025-09-12 13:54:55 +0000

    Remove Nth Node From End of List (Java)

--- problems/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ problems/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,22 +10,21 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode start = new ListNode();
-        start.next = head;
-        ListNode fast = start;
-        ListNode slow = start;     
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
 
-        for(int i = 1; i <= n; ++i)
-            fast = fast.next;
-    
-        while(fast.next != null)
-        {
-            fast = fast.next;
-            slow = slow.next;
+        for(int i=0; i<=n; i++){
+            first = first.next;
         }
-        
-        slow.next = slow.next.next;
-        
-        return start.next;
+
+        while(first != null){
+            first = first.next;
+            second = second.next;
+        }
+
+        second.next = second.next.next;
+        return dummy.next;
     }
 }
