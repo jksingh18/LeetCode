fccabe042d8c13fb01e7349b47d9cf9f3c7a21da
Jitesh Singh <65043216+jksingh18@users.noreply.github.com>
2025-11-21 15:47:04 +0000

    Next Greater Element I (Java)

--- problems/0496-next-greater-element-i/0496-next-greater-element-i.java
+++ problems/0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,19 +1,36 @@
+/*
+
+High-Level Idea:
+1. Instead of answering queries from nums1 directly, we:
+2. Precompute the next greater element for every element in nums2 using a monotonic stack in O(n).
+3. Store it in a Map<value, NGE> since all elements in nums2 are distinct.
+4. For each nums1[i], just do a map.get(nums1[i]) → O(1).
+5. So overall: O(n + m)
+
+*/
+
 class Solution {
-    public int[] nextGreaterElement(int[] findNums, int[] nums) {
-        int[] ret = new int[findNums.length];
-        Stack<Integer> stack = new Stack<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = nums.length - 1; i >= 0; i--) {
-            while(!stack.isEmpty() && stack.peek() <= nums[i]) {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> nextGreaterElementMap = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i=nums2.length-1; i>=0; i--){
+            int current = nums2[i];
+
+            while(!stack.isEmpty() && stack.peek() <= current){
                 stack.pop();
             }
-            if(stack.isEmpty()) map.put(nums[i], -1);
-            else map.put(nums[i], stack.peek());
-            stack.push(nums[i]);
+
+            int nextGreater = stack.isEmpty() ? -1 : stack.peek();
+            nextGreaterElementMap.put(current, nextGreater);
+            stack.push(current);
         }
-        for(int i = 0; i < findNums.length; i++) {
-            ret[i] = map.get(findNums[i]);
+
+        int[] result = new int[nums1.length];
+        for(int i=0; i<nums1.length; i++){
+            result[i] = nextGreaterElementMap.get(nums1[i]);
         }
-        return ret;
+
+        return result;
     }
 }
