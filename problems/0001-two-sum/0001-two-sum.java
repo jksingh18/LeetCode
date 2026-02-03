41c532cb24e8dfe9167a0e67775ebc02463d7f73
Jitesh Singh <65043216+jksingh18@users.noreply.github.com>
2026-02-03 12:56:27 +0000

    Two Sum (Java)

--- problems/0001-two-sum/0001-two-sum.java
+++ problems/0001-two-sum/0001-two-sum.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> twoSumMap = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(twoSumMap.containsKey(target - nums[i])){
-                return new int[]{twoSumMap.get(target - nums[i]), i};
+        Map<Integer, Integer> twoSumMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (twoSumMap.containsKey(target - nums[i])){
+                return new int[]{i, twoSumMap.get(target - nums[i])};
             }
             twoSumMap.put(nums[i], i);
         }
-        return new int[]{-1, -1};
+
+        return new int[]{};
     }
 }
