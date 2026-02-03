1b89e86f0596deadced11e2d43dac646de60a385
Jitesh Singh <65043216+jksingh18@users.noreply.github.com>
2026-02-03 13:02:17 +0000

    Two Sum II - Input Array Is Sorted (Java)

--- problems/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ problems/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,21 +1,20 @@
 class Solution {
-    // Use two-pointer, as the array is sorted, and the problem suggest to use O(1) Space Constraint
     public int[] twoSum(int[] numbers, int target) {
         int left = 0;
-        int right = numbers.length-1;
+        int right = numbers.length - 1;
 
-        while(left < right){
-            int sum = numbers[left] + numbers[right];
-            if(sum == target){
-                return new int[]{left+1, right+1};
-            } else if(sum < target){
-                left++;
-            } else{
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                return new int[]{left + 1, right + 1};
+            }
+
+            if (numbers[left] + numbers[right] > target) {
                 right--;
+            } else {
+                left++;
             }
         }
 
-        //this will never be reached, as the problem states that there will be 1 solution for sure.
-        return new int[]{-1, -1};
+        return new int[]{};
     }
 }
